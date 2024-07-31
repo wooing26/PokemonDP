6f3cb058cc26d9db532d37721082d7acd4284ba6
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 01:18:28 +0900

    최상위 Actor 클래스 생성

---
+++ Client/Actor.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "Actor.h"
+#include "InputManager.h"
+#include "TimeManager.h"
+
+Actor::Actor()
+{
+}
+
+Actor::~Actor()
+{
+}
+
+void Actor::BeginPlay()
+{
+}
+
+void Actor::Tick()
+{
+	
+}
+
+void Actor::Render(HDC hdc)
+{
+	
+}
---
+++ Client/Actor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class Texture;
+
+class Actor
+{
+public:
+	Actor();
+	virtual ~Actor();
+
+	virtual void	BeginPlay();
+	virtual void	Tick();
+	virtual void	Render(HDC hdc);
+
+	void			SetPos(Vec2 pos) { _pos = pos; }
+	Vec2			GetPos() { return _pos; }
+
+	void			SetLayer(LAYER_TYPE layer) { _layer = layer; }
+	LAYER_TYPE		GetLayer() { return _layer; }
+
+	
+
+protected:
+	Vec2		_pos = {0, 0};
+	Vec2		_destPos = { 0, 0 };
+	LAYER_TYPE	_layer = LAYER_OBJECT;
+
+};
+
