ddff1b5c8ece5ab46e668519970cd63f9454d5a3
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 01:19:49 +0900

    Scene Manager

---
+++ Client/SceneManager.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "SceneManager.h"
+#include "Scene.h"
+#include "BattleScene.h"
+
+void SceneManager::Init()
+{
+}
+
+void SceneManager::Update()
+{
+	if (_scene)
+		_scene->Update();
+}
+
+void SceneManager::Render(HDC hdc)
+{
+	if (_scene)
+		_scene->Render(hdc);
+}
+
+void SceneManager::Clear()
+{
+	SAFE_DELETE(_scene);
+}
+
+void SceneManager::ChangeScene(SceneType sceneType)
+{
+	if (_sceneType == sceneType)
+		return;
+
+	Scene* newScene = nullptr;
+	switch (sceneType)
+	{
+	case SceneType::DevScene:
+		break;
+	case SceneType::EditScene:
+		break;
+	case SceneType::BattleScene:
+		newScene = new BattleScene();
+		break;
+	}
+
+	_scene = newScene;
+	_sceneType = sceneType;
+
+	newScene->Init();
+}
