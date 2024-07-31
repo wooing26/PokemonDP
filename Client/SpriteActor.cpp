f5aefeb034b3cfb26ef525337d4b0bb0dad7fca2
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 01:20:55 +0900

    Sprite를 사용하는 Actor 생성

---
+++ Client/SpriteActor.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "SpriteActor.h"
+#include "Sprite.h"
+
+SpriteActor::SpriteActor()
+{
+}
+
+SpriteActor::~SpriteActor()
+{
+}
+
+void SpriteActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void SpriteActor::Tick()
+{
+	Super::Tick();
+}
+
+void SpriteActor::Render(HDC hdc)
+{
+	Super::Render(hdc);
+
+	Vec2Int size = _sprite->GetSize();
+
+	/*::BitBlt(hdc,
+		50,
+		50,
+		size.x,
+		size.y,
+		_sprite->GetDC(),
+		_sprite->GetPos().x,
+		_sprite->GetPos().y,
+		SRCCOPY);*/
+
+	::StretchBlt(hdc,
+		0,
+		0,
+		size.x * 4,
+		size.y * 4,
+		_sprite->GetDC(),
+		_sprite->GetPos().x,
+		_sprite->GetPos().y,
+		size.x,
+		size.y,
+		SRCCOPY);
+}
