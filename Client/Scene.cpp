ae003f8796015bbeb610299b39c1f51f3b0ed4f2
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 01:18:05 +0900

    최상위 Scene 클래스 생성

---
+++ Client/Scene.cpp
@@ -0,0 +1,70 @@
+#include "pch.h"
+#include "Scene.h"
+#include "Actor.h"
+#include "UI.h"
+
+Scene::Scene()
+{
+
+}
+
+Scene::~Scene()
+{
+	for (const vector<Actor*>& actors : _actors)
+		for (Actor* actor : actors)
+			SAFE_DELETE(actor);
+	
+	_actors->clear();
+
+	for (UI* ui : _uis)
+		SAFE_DELETE(ui);
+
+	_uis.clear();
+}
+
+void Scene::Init()
+{
+	for (const vector<Actor*>& actors : _actors)
+		for (Actor* actor : actors)
+			actor->BeginPlay();
+
+	for (UI* ui : _uis)
+		ui->BeginPlay();
+}
+
+void Scene::Update()
+{
+	for (const vector<Actor*>& actors : _actors)
+		for (Actor* actor : actors)
+			actor->Tick();
+
+	for (UI* ui : _uis)
+		ui->Tick();
+}
+
+void Scene::Render(HDC hdc)
+{
+	for (const vector<Actor*>& actors : _actors)
+		for (Actor* actor : actors)
+			actor->Render(hdc);
+
+	for (UI* ui : _uis)
+		ui->Render(hdc);
+}
+
+void Scene::AddActor(Actor* actor)
+{
+	if (actor == nullptr)
+		return;
+
+	_actors[actor->GetLayer()].push_back(actor);
+}
+
+void Scene::RemoveActor(Actor* actor)
+{
+	if (actor == nullptr)
+		return;
+
+	vector<Actor*>& v = _actors[actor->GetLayer()];
+	v.erase(std::remove(v.begin(), v.end(), actor), v.end());
+}
