affab65e010b72189ea04a543ce562b3e9a21743
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 01:16:40 +0900

    game 클래스

---
+++ Client/Game.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include "Game.h"
+#include "TimeManager.h"
+#include "InputManager.h"
+#include "SceneManager.h"
+#include "ResourceManager.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+	GET_SINGLE(SceneManager)->Clear();
+
+	_CrtDumpMemoryLeaks();				// 메모리 누수 찾기
+}
+
+void Game::Init(HWND hwnd)
+{
+	_hwnd = hwnd;
+	hdc = ::GetDC(hwnd);
+
+	::GetClientRect(hwnd, &_rect);
+
+	hdcBack = ::CreateCompatibleDC(hdc);									// hdc와 호환되는 DC 생성
+	_bmpBack = ::CreateCompatibleBitmap(hdc, _rect.right, _rect.bottom);	// hdc와 호환되는 비트맵 생성
+	HBITMAP prev = (HBITMAP)::SelectObject(hdcBack, _bmpBack);				// dcBack과 비트맵 연결
+	::DeleteObject(prev);
+
+	GET_SINGLE(TimeManager)->Init();
+	GET_SINGLE(InputManager)->Init(hwnd);
+	GET_SINGLE(SceneManager)->Init();
+	GET_SINGLE(ResourceManager)->Init(hwnd, L"..\\Resources");
+
+	GET_SINGLE(SceneManager)->ChangeScene(SceneType::BattleScene);
+}
+
+void Game::Update()
+{
+	GET_SINGLE(TimeManager)->Update();
+	GET_SINGLE(InputManager)->Update();
+	GET_SINGLE(SceneManager)->Update();
+}
+
+void Game::Render()
+{
+	GET_SINGLE(SceneManager)->Render(hdcBack);
+
+	uint32 fps = GET_SINGLE(TimeManager)->GetFPS();
+	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
+
+	{
+		POINT mousePos = GET_SINGLE(InputManager)->GetMousePos();		
+		wstring str = std::format(L"Mouse({0}, {1})", mousePos.x, mousePos.y);
+		Utils::DrawText(hdcBack, Vec2Int(10, 10), str);
+	}
+	{
+		wstring str = std::format(L"FPS({0}), DT({1})", fps, deltaTime);
+		Utils::DrawText(hdcBack, Vec2Int(550, 10), str);
+	}
+
+	// Double Buffering
+	::BitBlt(hdc, 0, 0, _rect.right, _rect.bottom, hdcBack, 0, 0, SRCCOPY);		// 비트 블릿 : 고속 복사
+	::PatBlt(hdcBack, 0, 0, _rect.right, _rect.bottom, WHITENESS);				// hdcBack을 흰식으로 만들어 줌
+}
