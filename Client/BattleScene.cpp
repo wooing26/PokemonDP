3b4d90bed3911c9f5d38553d30f728357f9b056c
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-01 17:32:08 +0900

    터치 스크린 배치 (크기가 달라서 일부가 잘림)

--- Client/BattleScene.cpp
+++ Client/BattleScene.cpp
@@ -18,8 +18,13 @@ void BattleScene::Init()
 {
 	Super::Init();
 	GET_SINGLE(ResourceManager)->LoadTexture(L"BattleBG", L"Sprite\\Background\\Battle Backgrounds.bmp");
+	GET_SINGLE(ResourceManager)->LoadTexture(L"TouchScreen", L"Sprite\\Background\\Touch Screen Backgrounds.bmp");
 
 	GET_SINGLE(ResourceManager)->CreateSprite(L"Battle_DryWater", GET_SINGLE(ResourceManager)->GetTexture(L"BattleBG"), 17, 17, 256, 144);
+	GET_SINGLE(ResourceManager)->CreateSprite(L"TS_DiamondOpening", GET_SINGLE(ResourceManager)->GetTexture(L"TouchScreen"), 46, 16, 256, 192);
+	GET_SINGLE(ResourceManager)->CreateSprite(L"TS_Battle", GET_SINGLE(ResourceManager)->GetTexture(L"TouchScreen"), 307, 410, 256, 192);
+	
+
 	{
 		Sprite* sprite = GET_SINGLE(ResourceManager)->GetSprite(L"Battle_DryWater");
 
@@ -27,6 +32,16 @@ void BattleScene::Init()
 		background->SetSprite(sprite);
 		background->SetLayer(LAYER_BACKGROUND);
 		background->SetPos(Vec2{ GWinSizeX / 4, GWinSizeY / 2 });
+		AddActor(background);
+	}
+
+	{
+		Sprite* sprite = GET_SINGLE(ResourceManager)->GetSprite(L"TS_Battle");
+
+		SpriteActor* background = new SpriteActor();
+		background->SetSprite(sprite);
+		background->SetLayer(LAYER_BACKGROUND);
+		background->SetPos(Vec2{ GWinSizeX * 3 / 4, GWinSizeY / 2 });
 		AddActor(background);
 	}
 }
