e945037b4e8d455d631c4e3ceb1c6fb49329a9e2
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-08-05 19:45:53 +0900

    포켓몬 타입

--- Client/Enums.h
+++ Client/Enums.h
@@ -31,3 +31,27 @@ enum class ObjectState
 	Idle,
 	Move
 };
+
+enum class PokeType
+{
+	Normal,
+	Fire,
+	Water,
+	Grass,
+	Electric,
+	Ice,
+	Fighting,
+	Poison,
+	Ground,
+	Flying,
+	Psychic,
+	Bug,
+	Rock,
+	Ghost,
+	Dragon,
+	Dark,
+	Steel,
+	Fairy,
+
+	End
+};
