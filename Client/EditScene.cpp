b9e068ae41841da7f9e61e0b7209f10fc488c6b9
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-09-05 21:38:28 +0900

    edit Scene

---
+++ Client/EditScene.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "EditScene.h"
+#include "InputManager.h"
+#include "ResourceManager.h"
+
+EditScene::EditScene()
+{
+}
+
+EditScene::~EditScene()
+{
+}
+
+void EditScene::Init()
+{
+	Super::Init();
+}
+
+void EditScene::Update()
+{
+	Super::Update();
+}
+
+void EditScene::Render(HDC hdc)
+{
+	Super::Render(hdc);
+}
+
+void EditScene::AddActor(Actor* actor)
+{
+}
+
+void EditScene::RemoveActor(Actor* actor)
+{
+}
---
+++ Client/EditScene.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Scene.h"
+
+class EditScene : public Scene
+{
+	using Super = Scene;
+public:
+	EditScene();
+	virtual ~EditScene();
+
+	virtual void	Init() override;
+	virtual void	Update() override;
+	virtual void	Render(HDC hdc) override;
+
+	virtual void	AddActor(Actor* actor) override;
+	virtual void	RemoveActor(Actor* actor) override;
+
+
+
+};
+
