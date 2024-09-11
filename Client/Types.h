de7042ea8ce20b3a1fe05ae37e47297c0eb618a5
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-09-12 02:25:10 +0900

    Vector 타입 변환

--- Client/Types.h
+++ Client/Types.h
@@ -15,6 +15,7 @@ struct Vector
 {
 	Vector() {}
 	Vector(float x, float y) : x(x), y(y) {}
+	Vector(int32 x, int32 y) : x((float)x), y((float)y) {}
 	Vector(POINT pt) : x((float)pt.x), y((float)pt.y) {}
 
 	Vector operator+(const Vector& other)
