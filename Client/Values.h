4e2dd16f5b1cc99addf8880b8b10dd21539f7583
wooing26 <104512612+wooing26@users.noreply.github.com>
2024-09-12 02:25:48 +0900

    전체화면 설정

--- Client/Values.h
+++ Client/Values.h
@@ -4,11 +4,13 @@ const int32 MainScreenRatio = 4;
 const int32 MainSizeX = 256 * MainScreenRatio;
 const int32 MainSizeY = 144 * MainScreenRatio;
 
-const int32 TouchScreenRatio = 3;
+const int32 TouchScreenRatio = 2;
 const int32 TouchSizeX = 256 * TouchScreenRatio;
 const int32 TouchSizeY = 192 * TouchScreenRatio;
 
-const int32 GWinSizeX = MainSizeX + TouchSizeX;	// 
-const int32 GWinSizeY = MainSizeY + TouchSizeY;	// 
+const int32 GWinSizeX = ::GetSystemMetrics(SM_CXSCREEN); 
+const int32 GWinSizeY = ::GetSystemMetrics(SM_CYSCREEN);
+/*const int32 GWinSizeX = MainSizeX + TouchSizeX;
+const int32 GWinSizeY = MainSizeY + TouchSizeY;*/ 
 
 const float PI = 3.1415926f;
